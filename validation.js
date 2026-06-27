8d18f1625fef89ea8ed9caadf0b0e08f10782e5d
adam_M <mamanedam11@gmail.com>
2026-06-27 23:34:57 +0000

    ajout de valiationPassword avec verification du mot de pass

---
+++ validation.js
@@ -0,0 +1,16 @@
+function validerPassword(valeur) {
+    if (!valeur) {
+        return { valide: false, message: "le mot de passe est requis" }
+
+    }
+    if (valeur.lenght <= 9) {
+        return { valide: false, message: "le mot de passe doit contenir au moins 9 caracteres" }
+    }
+    if (!/\d/.test(valeur)) {
+        return { valide: false, message: "le mot de passe doit contenir au moins un chiffre" }
+    }
+    if (!/[A-Z]/.test(valeur)) {
+        return { valide: false, message: "le mot de passe doit contenir au moins une lette majuscule" }
+    }
+    return { valide: true, message: "Mot de passe valide" }
+}
